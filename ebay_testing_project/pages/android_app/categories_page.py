9a812d2761f20e3b35ffdc14b54d1d565694e690
Emil <emil88337@gmail.com>
2024-05-27 22:22:15 +0300

    add allure

--- ebay_testing_project/pages/android_app/categories_page.py
+++ ebay_testing_project/pages/android_app/categories_page.py
@@ -1,18 +1,22 @@
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser
+import allure
 
 
 class CategoriesPage():
 
     def click_computers_tablets(self):
-        browser.element(
-            (AppiumBy.ANDROID_UIAUTOMATOR, f'new UiSelector().text("Computers/Tablets & Networking")')).click()
+        with allure.step('Нажать Computers/Tablets & Networking'):
+            browser.element(
+                (AppiumBy.ANDROID_UIAUTOMATOR, f'new UiSelector().text("Computers/Tablets & Networking")')).click()
 
     def click_3d_printers_supplies(self):
-        browser.element((AppiumBy.ANDROID_UIAUTOMATOR, f'new UiSelector().text("3D Printers & Supplies")')).click()
+        with allure.step('Нажать 3D Printers & Supplies'):
+            browser.element((AppiumBy.ANDROID_UIAUTOMATOR, f'new UiSelector().text("3D Printers & Supplies")')).click()
 
     def click_3d_printers(self):
-        browser.element((AppiumBy.ANDROID_UIAUTOMATOR, f'new UiSelector().text("3D Printers")')).click()
+        with allure.step('Нажать 3D Printers'):
+            browser.element((AppiumBy.ANDROID_UIAUTOMATOR, f'new UiSelector().text("3D Printers")')).click()
 
 
 categories_page = CategoriesPage()
