55c5388f7ffc7719a1d5fb7d4646831efcec5d8f
Emil <emil88337@gmail.com>
2024-06-04 12:33:59 +0300

    add search field

--- tests/android_app/test_recently_viewed_item.py
+++ tests/android_app/test_recently_viewed_item.py
@@ -19,7 +19,7 @@ from allure_commons.types import Severity
 def test_recently_viewed_item():
     home_page_app.open_app()
     home_page_app.click_search_field()
-    browser.element((AppiumBy.ID, 'com.ebay.mobile:id/search_src_text')).type('watch')
+    home_page_app.fill_search_field()
     home_page_app.сlick_item_in_drop_down()
     list_items_page.open_item()
     item_title = item_page.copy_item_name_to_variable()
